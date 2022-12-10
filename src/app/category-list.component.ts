d954b549fa3d43c2deeed89dc074cb36de9de70a
Huann Almeida <huannvictor@gmail.com>
2022-12-10 09:37:57 -0300

    0805

--- src/app/category-list.component.ts
+++ src/app/category-list.component.ts
@@ -1,7 +1,7 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input } from "@angular/core";
 
 @Component({
-  selector: 'mw-category-list',
+  selector: "mw-category-list",
   template: `
     <span class="label" *ngFor="let category of categories">
       {{ category }}
@@ -27,13 +27,8 @@ import { Component, Input } from '@angular/core';
         font-style: italic;
         width: max-content;
       }
-      span {
-        display: inline-block;
-        margin-right: 4px;
-        margin-bottom: 4px;
-      }
-    `
-  ]
+    `,
+  ],
 })
 export class CategoryListComponent {
   @Input() categories: string[];
