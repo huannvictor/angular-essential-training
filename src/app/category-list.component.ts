0922148fb6770273e954d3e3f7f016261ef2b8ec
Huann Almeida <huannvictor@gmail.com>
2022-12-10 09:16:47 -0300

    0803

--- src/app/category-list.component.ts
+++ src/app/category-list.component.ts
@@ -8,8 +8,22 @@ import { Component, Input } from "@angular/core";
     </span>
   `,
   styles: [
-    ``
-  ]
+    `
+      span {
+        display: inline-block;
+        margin-right: 4px;
+        margin-bottom: 4px;
+      }
+      .label {
+        background-color: #999;
+        color: #fff;
+        border-radius: 4px;
+        padding: 2px 6px;
+        font-style: italic;
+        width: max-content;
+      }
+    `,
+  ],
 })
 export class CategoryListComponent {
   @Input() categories: string[];
